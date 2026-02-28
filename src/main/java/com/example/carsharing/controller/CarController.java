a1e5b64a888ca7092780cf2fefc73d92ca50cc42
Alex <alex.gurianov.2007@gmail.com>
2026-02-28 15:02:14 +0300

    fix style

--- src/main/java/com/example/carsharing/controller/CarController.java
+++ src/main/java/com/example/carsharing/controller/CarController.java
@@ -3,8 +3,8 @@ package com.example.carsharing.controller;
 import com.example.carsharing.dto.CarCreateRequest;
 import com.example.carsharing.dto.CarResponse;
 import com.example.carsharing.service.CarService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -50,6 +53,7 @@ public class CarController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public CarResponse createCar(@RequestBody CarCreateRequest request) {
         return carService.createCar(request);
     }
