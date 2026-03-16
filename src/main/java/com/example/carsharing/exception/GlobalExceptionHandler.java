1a75b20200cf82a0712538d4301c07a726b355c8
Alex <alex.gurianov.2007@gmail.com>
2026-03-16 09:47:59 +0300

    move exception

--- src/main/java/com/example/carsharing/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/carsharing/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.example.carsharing.controller;
+package com.example.carsharing.exception;
 
-import com.example.carsharing.exception.ConflictException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
